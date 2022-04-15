c01942f9c9e887468ad59d3ab6e7115bc411e51d
Dom <Domalbano35@gmail.com>
2022-04-14 20:59:50 -0400

    changed routing path

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -3,16 +3,32 @@ let notePad = require("../db/db.json");
 const fs = require("fs")
 const { v4: uuidv4 } = require("uuid");
 
-router.get("/api/notes", (req, res) => res.json(notePad));
+router.get("/notes", (req, res) => res.send(notePad));
 
-router.post("/api/notes"), (req, res) => {
-  req.body.id = uuidv4()
-  notePad.push(req.body)
-  console.log({notePad});
-  fs.writeFile("../db/db.json", JSON.stringify(notePad), (err) => {
-    if (err) throw err;
+router.post("/notes", (req, res) => {
+
+  let iD = uuidv4();
+  let newNote = {
+    id: iD,
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  fs.readFile("../note-taker/db/db.json", "utf8", (err, data) =>{
+    if (err) {
+      throw (err)
+    }
+    const existingNotes = JSON.parse(data)
+
+    existingNotes.push(newNote)
   })
-  res.JSON.req.body
-};
+
+  fs.writeFile("../note-pad/db/db.json", JSON.stringify(notePad), (err) => {
+    if (err) {
+      throw (err)
+    };
+  })
+  res.send(existingNotes)
+});
 
 module.exports = router;
